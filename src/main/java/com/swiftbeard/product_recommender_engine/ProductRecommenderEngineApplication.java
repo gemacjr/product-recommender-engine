4ed1a5057a6a1c4c2e03549d09c364b45451d628
gemacjr <edmccormic@me.com>
2025-11-05 22:18:03 -0500

    initial commit

---
+++ src/main/java/com/swiftbeard/product_recommender_engine/ProductRecommenderEngineApplication.java
@@ -0,0 +1,13 @@
+package com.swiftbeard.product_recommender_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRecommenderEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRecommenderEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swiftbeard/product_recommender_engine/ProductRecommenderEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftbeard.product_recommender_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRecommenderEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
